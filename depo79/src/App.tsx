e1791f9f3805a647c5bc8744d566a6bd5d02ef45
Haeryz <haeriz42069@gmail.com>
2025-01-14 13:48:25 +0700

    test

--- depo79/src/App.tsx
+++ depo79/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/main/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/main/Footer'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Register/>}/>
     </Routes>
     <Footer/>
   </Box>
