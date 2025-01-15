6b051f37ce0fd306957b02b21a80d9d57b35f3d4
TwicePie <kknurarief@gmail.com>
2025-01-15 11:49:57 +0700

    Editing Navbar

--- depo79/src/components/main/Navbar.tsx
+++ depo79/src/components/main/Navbar.tsx
@@ -1,26 +1,63 @@
-import React from 'react'
-import { For, HStack, IconButton, Text, VStack} from "@chakra-ui/react"
-import { LuPhone } from "react-icons/lu"
+import React from 'react';
+import { Button, Flex, HStack, IconButton, Text, VStack, Spacer } from '@chakra-ui/react';
+import { LuPhone } from 'react-icons/lu';
+import { MdOutlineDarkMode } from 'react-icons/md';
 
 function Navbar() {
   return (
-    <HStack wrap="wrap" gap="8">
-      <For each={["xs"]}>
-        {(size) => (
-          <VStack key={size}>
-            <IconButton
-              aria-label="Search database"
-              variant="outline"
-              size={size}
-            >
-              <LuPhone />
-            </IconButton>
-            <Text textStyle="xs">{size}</Text>
-          </VStack>
-        )}
-      </For>
+    <HStack wrap="wrap" gap="8" margin={4} w="100%">
+      {/* Bagian kiri */}
+      <HStack>
+        <Text>DEPO 79</Text>
+      </HStack>
+
+      <Spacer />
+
+      {/* Bagian kanan */}
+      <HStack gap={4}>
+        <VStack>
+          <IconButton
+            aria-label="Dark Mode Icon"
+            variant="outline"
+            size="lg"
+          >
+            <MdOutlineDarkMode />
+          </IconButton>
+        </VStack>
+        <Button
+          textStyle="xs"
+          w={16}
+          h={11}
+          background="black"
+          color="white"
+          variant="outline"
+        >
+          Diskon
+        </Button>
+        <Button
+          textStyle="xs"
+          w={16}
+          h={11}
+          background="black"
+          color="white"
+          variant="outline"
+        >
+          Alamat
+        </Button>
+        <Button
+          textStyle="xs"
+          w={16}
+          h={11}
+          background="black"
+          color="white"
+          variant="outline"
+          mr={"10"}
+        >
+          Login
+        </Button>
+      </HStack>
     </HStack>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
