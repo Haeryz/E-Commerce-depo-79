7acfb69b4f2d4ca0dfcd297d33b79b20910bf5bc
TwicePie <kknurarief@gmail.com>
2025-01-15 14:04:54 +0700

    Home fixed

--- depo79/src/pages/Home.tsx
+++ depo79/src/pages/Home.tsx
@@ -4,39 +4,33 @@ import { useAuthStore } from "../store/auth";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const Home: React.FC = () => {
-  const { user, logout, isAuthenticated } = useAuthStore((state) => state); // Get user, logout, and isAuthenticated from the store
-  const navigate = useNavigate(); // Initialize the useNavigate hook
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/login"); // Navigate to login page if not authenticated
-    }
-  }, [isAuthenticated, navigate]); // Trigger re-render and navigate when isAuthenticated changes
-
-  const handleLogout = () => {
-    logout(); // Call the logout function from the store
-  };
-
-  if (!isAuthenticated) {
-    return (
-      <Box mt={20} maxW="sm" mx="auto" p={4} borderWidth={1} borderRadius="md">
-        <Text textAlign="center">Please log in to view your data.</Text>
-      </Box>
-    );
-  }
+  const { user, logout, isAuthenticated } = useAuthStore((state) => state);
+  const navigate = useNavigate();
 
   return (
     <Box mt={20} maxW="sm" mx="auto" p={4} borderWidth={1} borderRadius="md">
-      <Text fontSize="2xl" fontWeight="bold" mb={4} textAlign="center">
-        Welcome, {user?.name}
-      </Text>
-
-      <Stack align="center">
-        <Text>Email: {user?.email}</Text>
-        <Button onClick={handleLogout} colorScheme="red">
-          Logout
-        </Button>
-      </Stack>
+      {isAuthenticated ? (
+        // Tampilan untuk user yang sudah login
+        <>
+          <Text fontSize="2xl" fontWeight="bold" mb={4} textAlign="center">
+            Welcome, {user?.name}
+          </Text>
+          <Stack align="center">
+            <Text>Email: {user?.email}</Text>
+            <Button onClick={logout} colorScheme="red">
+              Logout
+            </Button>
+          </Stack>
+        </>
+      ) : (
+        // Tampilan untuk user yang belum login
+        <Stack align="center">
+          <Text>Welcome to Our Website</Text>
+          <Button onClick={() => navigate("/login")} colorScheme="blue">
+            Login
+          </Button>
+        </Stack>
+      )}
     </Box>
   );
 };
