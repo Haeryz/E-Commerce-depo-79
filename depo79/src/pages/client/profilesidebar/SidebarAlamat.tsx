5e4689182ce4e1e785dd47a3ac035d0d176257c9
Haeryz <haeriz42069@gmail.com>
2025-01-17 10:52:27 +0700

    kawi mengawi

--- depo79/src/pages/client/profilesidebar/SidebarAlamat.tsx
+++ depo79/src/pages/client/profilesidebar/SidebarAlamat.tsx
@@ -1,10 +1,50 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Input, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { LuMapPin } from 'react-icons/lu'
+import { Field } from '../../../components/ui/field'
+import { NativeSelectField, NativeSelectRoot } from '../../../components/ui/native-select'
 
 function SidebarAlamat() {
-  return (
-    <Box p={10}>SidebarAlamat</Box>
-  )
+    return (
+        <Box >
+            <VStack align={"start"}>
+                <HStack gapX={10}>
+                    <LuMapPin size={40} />
+                    <Text fontWeight={"bold"} fontSize={"4xl"}>
+                        INFORMASI ALAMAT
+                    </Text>
+                </HStack>
+                <VStack mt={10} align={"start"} gapY={3}>
+                    <Field label="Alamat lengkap" maxW={"100%"} bgSize={"xl"}>
+                        <Input placeholder='Jalan, No, RT/RW (deskripsi)' />
+                    </Field>
+                    <Field label="Nomor Telefon" maxW={"40%"} >
+                        <NativeSelectRoot>
+                            <NativeSelectField items={["Pria", "Perempuan"]} />
+                        </NativeSelectRoot>
+                    </Field>
+                    <Field label="Nomor Telefon" maxW={"40%"} >
+                        <NativeSelectRoot>
+                            <NativeSelectField items={["Pria", "Perempuan"]} />
+                        </NativeSelectRoot>
+                    </Field>
+                    <Field label="Nomor Telefon" maxW={"40%"} >
+                        <NativeSelectRoot>
+                            <NativeSelectField items={["Pria", "Perempuan"]} />
+                        </NativeSelectRoot>
+                    </Field>
+                    <Field label="Nomor Telefon" maxW={"40%"} >
+                        <NativeSelectRoot>
+                            <NativeSelectField items={["Pria", "Perempuan"]} />
+                        </NativeSelectRoot>
+                    </Field>
+                    <Field label="Alamat lengkap" maxW={"40%"} >
+                        <Input placeholder='Jalan, No, RT/RW (deskripsi)' />
+                    </Field>
+                </VStack>
+            </VStack>
+        </Box>
+    )
 }
 
 export default SidebarAlamat
--- depo79/src/pages/client/profilesidebar/SidebarProfile.tsx
+++ depo79/src/pages/client/profilesidebar/SidebarProfile.tsx
@@ -1,9 +1,50 @@
+import { Box, HStack, Input, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { LuMapPin } from "react-icons/lu";
+import { Avatar } from '../../../components/ui/avatar';
+import { Field } from '../../../components/ui/field';
+import { NativeSelectField, NativeSelectRoot } from '../../../components/ui/native-select';
 
 function SidebarProfile() {
-  return (
-    <div>SidebarProfile</div>
-  )
+    return (
+        <Box>
+            <VStack align={"start"}>
+                <HStack gapX={10}>
+                    <LuMapPin size={40} />
+                    <Text fontWeight={"bold"} fontSize={"4xl"}>
+                        INFORMASI PRIBADI
+                    </Text>
+                </HStack>
+                <HStack mt={10} pl={10} pr={10} gapX={20}>
+                    <Avatar
+                        name='Mewing ohio'
+                        src='https://bit.ly/broken-link'
+                        colorPalette={"teal"}
+                        size={"2xl"}
+                    />
+                    <Text fontWeight={"bold"} fontSize={"2xl"}>
+                        Mewing Ohio
+                    </Text>
+                </HStack>
+            </VStack>
+            <VStack mt={10} align={"start"} gapY={5}>
+                <Field label="Email" maxW={"40%"} invalid errorText="Invalid email">
+                    <Input placeholder='me@example.com' />
+                </Field>
+                <Field label="Password" maxW={"40%"} >
+                    <Input placeholder='me@example.com' />
+                </Field>
+                <Field label="Nomor Telefon" maxW={"40%"} >
+                    <Input placeholder='me@example.com' />
+                </Field>
+                <Field label="Jenis Kelamin" maxW={"40%"} >
+                    <NativeSelectRoot>
+                        <NativeSelectField items={["Pria", "Perempuan"]} />
+                    </NativeSelectRoot>
+                </Field>
+            </VStack>
+        </Box>
+    )
 }
 
 export default SidebarProfile
