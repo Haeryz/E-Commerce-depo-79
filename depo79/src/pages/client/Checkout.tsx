a88a613e7aee173dad0eaae02351d13a3117ab52
TwicePie <kknurarief@gmail.com>
2025-01-31 11:22:47 +0700

    Implement location selection in Checkout component with Zustand store integration for provinces, cities, districts, and villages

--- depo79/src/pages/client/Checkout.tsx
+++ depo79/src/pages/client/Checkout.tsx
@@ -1,13 +1,78 @@
-import { Box, HStack, Input, Separator, Text, Textarea, VStack, Stack, Spinner } from '@chakra-ui/react'
+import { Box, HStack, Input, Separator, Text, Textarea, VStack, Stack, Spinner, createListCollection } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { BreadcrumbCurrentLink, BreadcrumbLink, BreadcrumbRoot } from '../../components/ui/breadcrumb'
 import { Field } from '../../components/ui/field'
 import { Button } from '../../components/ui/button'
 import { useCartStore } from '../../store/cart';
+import { useLokasiStore } from '../../store/location';
+import { SelectContent, SelectItem, SelectLabel, SelectRoot, SelectTrigger, SelectValueText } from '../../components/ui/select'
 
 
 function Checkout() {
   const [loading, setLoading] = useState(true);
+  const [selectedProvinsi, setSelectedProvinsi] = useState<number>(0);
+  const [selectedKota, setSelectedKota] = useState<number>(0);
+  const [selectedKecamatan, setSelectedKecamatan] = useState<number>(0);
+
+  const { provinsi, fetchProvinsi, kota, fetchKota, kecamatan, fetchKecamatan, kelurahan, fetchKelurahan } = useLokasiStore();
+
+  useEffect(() => {
+    const initializeProvinsi = async () =>
+    {
+      try {
+        setLoading(true);
+        await fetchProvinsi();
+      } catch (error) {
+        console.error('Error fetching provinsi:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    initializeProvinsi();
+  }, [fetchProvinsi]);
+
+  const provinsiCollection = createListCollection({
+    items: provinsi.map(prov => ({
+      label: prov.nama,
+      value: prov._id
+    }))
+  })
+
+  const kotaCollection = createListCollection({
+    items: kota.map(k => ({
+      label: k.nama,
+      value: k._id
+    }))
+  })
+
+  const kecamatanCollection = createListCollection({
+    items: kecamatan.map(kec => ({
+      label: kec.nama,
+      value: kec._id
+    }))
+  })
+
+  const kelurahanCollection = createListCollection({
+    items: kelurahan.map(kel => ({
+      label: kel.nama,
+      value: kel._id
+    }))
+  })
+
+  const handleKecamatanChange = (value: number) => {
+    setSelectedKecamatan(value);
+    fetchKelurahan(parseInt(value.toString()));
+  }
+  
+  const handleKotaChange = (value: number) => {
+    setSelectedKota(value);
+    fetchKecamatan(parseInt(value.toString()));
+  }
+
+  const handleProvinsiChange = (value: number) => {
+    setSelectedProvinsi(value);
+    fetchKota(parseInt(value.toString()));
+  }
 
   const { items, total, fetchCart } = useCartStore();
 
@@ -44,18 +109,18 @@ function Checkout() {
         </BreadcrumbLink>
       </BreadcrumbRoot>
 
-      <Stack 
-        direction={['column', 'column', 'row']} 
-        gap={[4, 6, 10]} 
-        w="full" 
+      <Stack
+        direction={['column', 'column', 'row']}
+        gap={[4, 6, 10]}
+        w="full"
         px={[4, 6, 10]}
       >
-        <Box 
-          bg="bg" 
-          shadow="md" 
-          borderRadius="md" 
-          width={['100%', '100%', '70%']} 
-          mb={5} 
+        <Box
+          bg="bg"
+          shadow="md"
+          borderRadius="md"
+          width={['100%', '100%', '70%']}
+          mb={5}
           boxShadow="0px 8px 20px 8px rgba(0, 0, 0, 0.2)"
         >
           <VStack align="start" m={[3, 4, 5]}>
@@ -65,9 +130,9 @@ function Checkout() {
             <Field label="Nama Lengkap" w="full">
               <Input placeholder="me@example.com" />
             </Field>
-            <Stack 
-              direction={['column', 'column', 'row']} 
-              w="full" 
+            <Stack
+              direction={['column', 'column', 'row']}
+              w="full"
               gap={4}
             >
               <Field label="Email" w="full">
@@ -83,29 +148,77 @@ function Checkout() {
             <Field label="Alamat Lengkap" w="full">
               <Textarea size="xl" placeholder="alamat lengkap" />
             </Field>
-            <Stack 
-              direction={['column', 'column', 'row']} 
-              w="full" 
+            <Stack
+              direction={['column', 'column', 'row']}
+              w="full"
               gap={4}
             >
-              <Field label="Provinsi" w="full">
-                <Input placeholder="provinsi" />
-              </Field>
-              <Field label="Kota" w="full">
-                <Input placeholder="kota" />
-              </Field>
+              <SelectRoot collection={provinsiCollection} size="md" width="100%">
+                <SelectLabel>
+                  Pilih Provinsi
+                </SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder='Pilih Provinsi'/>
+                </SelectTrigger>
+                <SelectContent>
+                  {provinsiCollection.items.map((prov) => (
+                    <SelectItem item={prov} key={prov.value} onClick={() => handleProvinsiChange(prov.value)}>
+                      {prov.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
+              <SelectRoot collection={kotaCollection} size="md" width="100%">
+                <SelectLabel>
+                  Pilih Kota
+                </SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder='Pilih Kota'/>
+                </SelectTrigger>
+                <SelectContent>
+                  {kotaCollection.items.map((kot) => (
+                    <SelectItem item={kot} key={kot.value} onClick={() => handleKotaChange(kot.value)}>
+                      {kot.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
             </Stack>
-            <Stack 
-              direction={['column', 'column', 'row']} 
-              w="full" 
+            <Stack
+              direction={['column', 'column', 'row']}
+              w="full"
               gap={4}
             >
-              <Field label="Kecamatan" w="full">
-                <Input placeholder="kecamatan" />
-              </Field>
-              <Field label="Kelurahan" w="full">
-                <Input placeholder="kelurahan" />
-              </Field>
+              <SelectRoot collection={kecamatanCollection} size="md" width="100%">
+                <SelectLabel>
+                  Kecamatan
+                </SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder='Pilih Kecamatan'/>
+                </SelectTrigger>
+                <SelectContent>
+                  {kecamatanCollection.items.map((prov) => (
+                    <SelectItem item={prov} key={prov.value} onClick={() => handleKecamatanChange(prov.value)}>
+                      {prov.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
+              <SelectRoot collection={kelurahanCollection} size="md" width="100%">
+                <SelectLabel>
+                  Kelurahan
+                </SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder='Pilih Kelurahan'/>
+                </SelectTrigger>
+                <SelectContent>
+                  {kelurahanCollection.items.map((kel) => (
+                    <SelectItem item={kel} key={kel.value}>
+                      {kel.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
             </Stack>
             <Field label="Kodepos" w="full">
               <Input placeholder="kodepos" />
@@ -113,11 +226,11 @@ function Checkout() {
           </VStack>
         </Box>
 
-        <Box 
-          bg="bg" 
-          shadow="md" 
-          borderRadius="md" 
-          width={['100%', '100%', '30%']} 
+        <Box
+          bg="bg"
+          shadow="md"
+          borderRadius="md"
+          width={['100%', '100%', '30%']}
           alignSelf={['center', 'center', 'flex-start']}
         >
           <VStack>
@@ -127,9 +240,9 @@ function Checkout() {
                 <Text>Rp.{(item.product.harga_jual * item.quantity).toLocaleString('id-ID')}</Text>
               </HStack>
             ))}
-            
+
             <Separator />
-            
+
             <HStack justifyContent="space-between" w="full" mt={5}>
               <Text ml={5} fontWeight="Bold" color="gray.500">
                 Subtotal
